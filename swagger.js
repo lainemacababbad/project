e6e7e87ecd5d3a9241f0a2ee0e4899e78166b8a0
lainemacababbad <149907223+lainemacababbad@users.noreply.github.com>
2024-11-17 20:09:34 -0700

    Updated server on swagger.js

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: 'http://localhost:3000/',
+        url: 'https://cse341-project-6k9u.onrender.com/',
       },
     ],
     components: {
