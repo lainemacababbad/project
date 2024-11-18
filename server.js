8dad634cdf992011e2f57b65a974ff81026c3980
lainemacababbad <149907223+lainemacababbad@users.noreply.github.com>
2024-11-17 21:02:45 -0700

    removed the google authentication

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const mongodb = require("./db/connect");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./swagger");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const path = require("path");
@@ -46,22 +44,6 @@ mongodb.initDb((err, mongodbInstance) => {
     saveUninitialized: true,
     store: MongoStore.create({ clientPromise: mongodbInstance.client.connect() }),
   }));
-
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://cse341-project-6k9u.onrender.com/auth/google/callback"
-  }, (accessToken, refreshToken, profile, done) => done(null, profile)));
-
-  passport.serializeUser((user, done) => done(null, user));
-  passport.deserializeUser((user, done) => done(null, user));
-
-  app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-  app.get("/auth/google/callback", passport.authenticate("google", { failureRedirect: "/" }), (req, res) => res.redirect("/"));
-  app.get("/login", (req, res) => res.send('<h1>Login Page</h1><a href="/auth/google">Login with Google</a>'));
 
   app.listen(port, () => {
     console.log(`Connected to DB and listening on ${port}`);
